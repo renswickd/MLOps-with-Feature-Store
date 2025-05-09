62374e1a648c14f7c68707dc06e31b8620cb7c67
renswickd <renswick.delver@gmail.com>
2025-05-09 22:04:07 +1200

    Model training with Hyperparameter tuning

---
+++ config/hyperparameters.py
@@ -0,0 +1,6 @@
+parameter_lists = {
+                    'n_estimators': [100, 200, 300],
+                    'max_depth': [10, 20, 30],
+                    'min_samples_split': [2, 5],
+                    'min_samples_leaf': [1, 2]
+                }
